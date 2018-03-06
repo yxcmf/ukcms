696f309567d69633d14e9b2a344d9bbce911ed7d
ukcms <yxcms@qq.com>
2018-03-06 22:15:06 +0800

    优化路由规则

--- route/route.php
+++ route/route.php
@@ -3,12 +3,12 @@
 switch (DEFAULT_MODULE) {
     //index.php入口路由
     case 'home':
-        Route::rule('/', 'home/index/index');
         Route::rule(':name/:id$', 'home/column/content')->pattern(['name' => '[a-zA-Z]+', 'id' => '\d+']);
         Route::rule('search', 'home/index/search');
         Route::rule('captcha/[:id]', "\\think\\captcha\\CaptchaController@index");
         Route::rule('single/index/<id>', 'home/single/index')->pattern(['id' => '\d+']);
         Route::rule('Sitemap.xml', 'home/Seo/Sitemap');
         Route::rule('<name>/<condition?>-<page?>', 'home/column/index')->pattern(['name' => '[a-zA-Z]+', 'condition' => '[0-9_&=a-zA-Z]+', 'page' => '\d+']);
+        Route::rule('/', 'home/index/index');
         break;
 }
